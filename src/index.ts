1a79e1cd5c2df323994eb8e3dec44da719fa9a78
Libor Vachal <lipelix@gmail.com>
2022-03-13 11:30:46 +0100

    feat: create basic express server

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { processData as processDataRevolut} from './feeders/revolutFeeder'
+import { processData as processDataCsob } from './feeders/csobFeeder'
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/revolut', async (req, res) => {
+  const respon = await processDataRevolut()
+  res.send(respon)
+})
+
+app.get('/csob', async (req, res) => {
+  const respon = await processDataCsob()
+  res.send(respon)
+})
+
+app.get('/', async (req, res) => {
+  res.send('Welcome on Curzy!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`)
+})
