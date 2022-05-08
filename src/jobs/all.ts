efaa9e2cfd6bc2aa1c73683f5bc325b96421fe00
Libor Vachal <libor.vachal@gmail.com>
2022-05-08 16:55:52 +0200

    feat: add sepa for airbank

--- src/jobs/all.ts
+++ src/jobs/all.ts
@@ -1,13 +1,15 @@
 import { processData as processDataCsob } from '../feeders/csobFeeder'
 import { processData as processDataRevolut } from '../feeders/revolutFeeder'
-import { processData as processDataAirbank } from '../feeders/airbankFeeder'
+import { processData as processDataAirbankCard } from '../feeders/airbankCardFeeder'
+import { processData as processDataAirbankSepa } from '../feeders/airbankSepaFeeder'
 import { run } from './storeRates'
 
 (async () => {
   try {
     await run(processDataCsob)
     await run(processDataRevolut)
-    await run(processDataAirbank)
+    await run(processDataAirbankCard)
+    await run(processDataAirbankSepa)
     process.exit(0);
   } catch (err) {
     process.exit(1);
