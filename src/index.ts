fbed2b4309ba92c2480d55a0f89ed7b6de6b5f49
Libor Váchal <libor.vachal@heureka.cz>
2025-03-17 21:33:40 +0100

    chore: remove origin cors

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import { initializeDbConnection } from './db/init'
-import rates from './api/rates'
-import fees from './api/fees'
-import allJobsRouter from './jobs/all'
+import express from "express";
+import cors from "cors";
+import { initializeDbConnection } from "./db/init";
+import rates from "./api/rates";
+import fees from "./api/fees";
+import allJobsRouter from "./jobs/all";
 
-const corsOptions = {
-  origin: 'https://curzy.herokuapp.com',
-}
-
-const app = express()
+const app = express();
 const PORT = process.env.PORT;
 
 (async (app: express.Application) => {
-  await initializeDbConnection(app)
-})(app)
+  await initializeDbConnection(app);
+})(app);
 
-app.use(cors(corsOptions))
-app.use('/api/rates', rates)
-app.use('/api/fees', fees)
-app.use('/jobs/all', allJobsRouter)
+app.use("/api/rates", rates);
+app.use("/api/fees", fees);
+app.use("/jobs/all", allJobsRouter);
 
-app.get('/', async (req, res) => {
-  res.send('Welcome on Curzy!')
-})
+app.get("/", async (req, res) => {
+  res.send("Welcome on Curzy!");
+});
 
 app.listen(PORT, () => {
-  console.log(`🚀 Curzy app listening on PORT ${PORT}`)
-})
+  console.log(`🚀 Curzy app listening on PORT ${PORT}`);
+  console.log(
+    `🚀 Deployment metadata: ${JSON.stringify({
+      GAE_APPLICATION: process.env.GAE_APPLICATION,
+      GAE_DEPLOYMENT_ID: process.env.GAE_DEPLOYMENT_ID,
+      GAE_ENV: process.env.GAE_ENV,
+      GAE_INSTANCE: process.env.GAE_INSTANCE,
+      GAE_MEMORY_MB: process.env.GAE_MEMORY_MB,
+      GAE_RUNTIME: process.env.GAE_RUNTIME,
+      GAE_SERVICE: process.env.GAE_SERVICE,
+      GAE_VERSION: process.env.GAE_VERSION,
+      GOOGLE_CLOUD_PROJECT: process.env.GOOGLE_CLOUD_PROJECT,
+      NODE_ENV: process.env.NODE_ENV,
+    })}`
+  );
+});
