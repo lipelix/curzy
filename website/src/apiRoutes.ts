ea4ee094e59feee290ee3dcef65d3e3f0631dbec
Libor Váchal <libor.vachal@heureka.cz>
2025-04-19 12:59:09 +0200

    feat: add cors to restrict api access and enable local dev

--- website/src/apiRoutes.ts
+++ website/src/apiRoutes.ts
@@ -1,6 +1,6 @@
 export const ROUTES = (() => {
   return {
-    RATES: `${process.env.REACT_APP_API_HOST}/api/rates`,
-    FEES: `${process.env.REACT_APP_API_HOST}/api/fees`,
+    RATES: `${process.env.REACT_APP_API_HOST}/v1/rates`,
+    FEES: `${process.env.REACT_APP_API_HOST}/v1/fees`,
   }
 })()
